877e2ddffd2b26817685a4bcce9091fefe289449
Miguel Flórez <miguelflorez@MacBook-Air-de-Miguel.local>
2021-08-12 12:01:27 +0200

    book controller file added

---
+++ src/book_controller.py
@@ -0,0 +1,39 @@
+from db import get_db
+
+def insert_book(name, author):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "INSERT INTO books(name, author) VALUES (?, ?)"
+    cursor.execute(statement, [name, author])
+    db.commit()
+    return True
+
+def update_book(id, name, author):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "UPDATE books SET name = ?, author = ? WHERE id = ?"
+    cursor.execute(statement, [name, author, id])
+    db.commit()
+    return True
+
+def delete_book(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM books WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
+
+def get_by_id(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT id, name, author FROM books WHERE id = ?"
+    cursor.execute(statement, [id])
+    return cursor.fetchone()
+
+def get_books():
+    db = get_db()
+    cursor = db.cursor()
+    query = "SELECT id, name, author FROM books"
+    cursor.execute(query)
+    return cursor.fetchall()
