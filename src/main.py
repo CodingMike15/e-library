a6190810d42b73d2ada1ddc6163a493ecedec00f
Miguel Flórez <miguelflorez@MacBook-Air-de-Miguel.local>
2021-08-13 22:54:04 +0200

    insert book finished

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from db import create_tables
 
 app = Flask(__name__)
 
-@app.route('/books', methods=["GET"])
+@app.route('/', methods=["GET"])
 def get_books():
     books = book_controller.get_books()
     return render_template('get_books.html', books=books)
 
-@app.route('/book/', methods=['GET', 'POST'])
+@app.route('/findbookbyid', methods=['GET', 'POST'])
 def get_book_by_id():
     if request.method == 'GET':
         return render_template('find_book_by_id.html')
@@ -19,13 +19,16 @@ def get_book_by_id():
         book = book_controller.get_by_id(book_id)
         return render_template('find_book_by_id.html', book=book)
 
-@app.route('/book', methods=['POST'])
+@app.route('/insertbook', methods=['GET', 'POST'])
 def insert_book():
-    book_details = request.get_json()
-    name = book_details['name']
-    author = book_details['author']
-    result = book_controller.insert_book(name, author)
-    return jsonify(result)
+    if request.method == 'GET':
+        return render_template('insert_book.html')
+
+    if request.method == 'POST':
+        name = request.form['name']
+        author = request.form['author']
+        result = book_controller.insert_book(name, author)
+        return render_template('insert_book.html')
 
 @app.route('/book/<id>', methods=['DELETE'])
 def delete_book(id):
